6d14cb4ad46336e6ff78ab0e963a316b5cbf4251
Rafhael <rafhaelpillaca2001@gmail.com>
2023-08-31 20:49:40 -0500

    Initial Commit

---
+++ instaCloneJue/instaCloneJueApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaCloneJueApp.swift
+//  instaCloneJue
+//
+//  Created by Rafhael Pillaca Pariona on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct instaCloneJueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
